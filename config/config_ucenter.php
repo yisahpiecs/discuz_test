8582f2609b6e41b14ecb422495bc95b96516d995
test <test@example.com>
2022-07-18 18:07:55 +0800

    fix err

---
+++ config/config_ucenter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', '20.60.250.234');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'mydainli@123!');
+define('UC_DBNAME', '国网江西省电力有限公司');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`discuz`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'T3Z7V6w2U0vf56t9xcbcI0v9YfHaffk0ScJ4u277j6z7Ffg2RcA1i5m8v3haH0ue');
+define('UC_API', 'http://x');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
